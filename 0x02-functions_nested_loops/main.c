eaf18e319fb8367d07accc45837cb847dea01685
kayrop75 <kropbrayo@gmail.com>
2023-07-18 23:02:41 +0300

    my commit

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+int _putchar(char c)
+{
+	    /* Your implementation of _putchar goes here */
+}
+
+int main(void)
+{
+	    print_alphabet();
+	        _putchar('\n');
+
+		    return 0;
+}
+
