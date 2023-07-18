2f129792a9a8eac87ac69b1e2be61f3589ad5921
kayrop75 <kropbrayo@gmail.com>
2023-07-18 22:15:57 +0300

    nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+	    char word[] = "_putchar";
+	        int length = sizeof(word) - 1;
+
+		    for (int i = 0; i < length; i++)
+			        {
+					        for (int j = 0; j <= i; j++)
+							        {
+									            putchar(word[j]);
+										            }
+						    }
+
+		        putchar('\n');
+
+			    return 0;
+}
+
