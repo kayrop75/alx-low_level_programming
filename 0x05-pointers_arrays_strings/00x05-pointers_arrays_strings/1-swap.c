0ae9685c97f0a5c5bdaf1124deb2e82a9b3ad80a
kayrop75 <kropbrayo@gmail.com>
2023-07-24 23:26:58 +0300

    reset to 98 has a main.h file

---
+++ 0x05-pointers_arrays_strings/00x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *  * swap_int - Swaps the values of two integers.
+ *   * @a: Pointer to the first integer.
+ *    * @b: Pointer to the second integer.
+ *     */
+void swap_int(int *a, int *b)
+{
+	    int temp = *a; // Store the value of the first integer in a temporary variable
+			   //     *a = *b;       // Assign the value of the second integer to the first integer
+			   //         *b = temp;     // Assign the value of the temporary variable (first integer's original value) to the second integer
+			   //         }
+			   //
