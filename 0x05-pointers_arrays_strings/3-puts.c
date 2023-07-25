ed8a8cc4c2b5af4904ceda0eccada3b08e272466
kayrop75 <kropbrayo@gmail.com>
2023-07-25 18:37:20 +0300

    what the hell

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,21 +1,21 @@
 #include "main.h"
-#include <stdio.h> // Include the standard I/O library for putchar function
-		   //
-		   // /**
-		   //  * _puts - Prints a string, followed by a new line, to stdout.
-		   //   * @str: Pointer to the string.
-		   //    */
-		   //    void _puts(char *str)
-		   //    {
-		   //        int i = 0;
-		   //
-		   //            /* Loop through the string and print each character */
-		   //                while (str[i] != '\0')
-		   //                    {
-		   //                            putchar(str[i]); // Use putchar instead of _putchar
-		   //                                    i++;
-		   //                                        }
-		   //
-		   //                                            putchar('\n'); // Print a new line after printing the characters
-		   //                                            }
-		   //
+#include <stdio.h>
+
+/**
+ *  * _puts - Prints a string, followed by a new line, to stdout.
+ *   * @str: Pointer to the string.
+ *    */
+void _puts(char *str)
+{
+	    int i = 0;
+
+	        /* Loop through the string and print each character */
+	        while (str[i] != '\0')
+			    {
+				            putchar(str[i]); /* Use putchar instead of _putchar */
+					            i++;
+						        }
+
+		    putchar('\n'); /* Print a new line after printing the characters */
+}
+
