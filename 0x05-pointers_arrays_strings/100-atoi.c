c31cedd8fe666161b6f57e0c121dc877dae8d3f5
kayrop75 <kropbrayo@gmail.com>
2023-07-25 23:36:33 +0300

    what the hell

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,48 +1,54 @@
 #include "main.h"
 
+/**
+ *  * _atoi - Converts a string to an integer.
+ *   * @s: The string to convert.
+ *    *
+ *     * Return: The converted integer.
+ *      */
 int _atoi(char *s)
 {
-	    int sign = 1;
-	        int result = 0;
-		    int i = 0;
+	int sign = 1;
+	int result = 0;
+	int i = 0;
 
-		        // Skip leading whitespace
-			//     while (s[i] == ' ')
-			//         {
-			//                 i++;
-			//                     }
-			//
-			//                         // Check for sign
-			//                             if (s[i] == '-')
-			//                                 {
-			//                                         sign = -1;
-			//                                                 i++;
-			//                                                     }
-			//                                                         else if (s[i] == '+')
-			//                                                             {
-			//                                                                     i++;
-			//                                                                         }
-			//
-			//                                                                             // Convert string to integer
-			//                                                                                 while (s[i] >= '0' && s[i] <= '9')
-			//                                                                                     {
-			//                                                                                             // Check for overflow
-			//                                                                                                     if (result > (INT_MAX / 10) || (result == (INT_MAX / 10) && (s[i] - '0') > (INT_MAX % 10)))
-			//                                                                                                             {
-			//                                                                                                                         if (sign == 1)
-			//                                                                                                                                     {
-			//                                                                                                                                                     return INT_MAX;
-			//                                                                                                                                                                 }
-			//                                                                                                                                                                             else
-			//                                                                                                                                                                                         {
-			//                                                                                                                                                                                                         return INT_MIN;
-			//                                                                                                                                                                                                                     }
-			//                                                                                                                                                                                                                             }
-			//
-			//                                                                                                                                                                                                                                     result = (result * 10) + (s[i] - '0');
-			//                                                                                                                                                                                                                                             i++;
-			//                                                                                                                                                                                                                                                 }
-			//
-			//                                                                                                                                                                                                                                                     return result * sign;
-			//                                                                                                                                                                                                                                                     }
-			//
+	/* Skip leading whitespace */
+	while (s[i] == ' ')
+	{
+		i++;
+	}
+
+	/* Check for sign */
+	if (s[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	else if (s[i] == '+')
+	{
+		i++;
+	}
+
+	/* Convert string to integer */
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		/* Check for overflow */
+		if (result > (INT_MAX / 10) || (result == (INT_MAX / 10) && (s[i] - '0') > (INT_MAX % 10)))
+		{
+			if (sign == 1)
+			{
+				return INT_MAX;
+			}
+			else
+			{
+				return INT_MIN;
+			}
+		}
+
+		result = (result * 10) + (s[i] - '0');
+		i++;
+	}
+
+	return result * sign;
+}
+
--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -12,25 +12,26 @@ int main(void)
 	    int sum = 0;
 	        int diff;
 		    char password[100];
+		        int i;
 
-		        srand(time(NULL));
+			    srand(time(NULL));
 
-			    for (int i = 0; i < 100; i++)
-				        {
-						        password[i] = rand() % 94 + 33;
-							        sum += password[i];
+			        for (i = 0; i < 100; i++)
+					    {
+						            password[i] = rand() % 94 + 33;
+							            sum += password[i];
 
-								        if (sum >= 2772)
-										        {
-												            diff = sum - 2772;
-													                password[i] -= diff;
-															            password[i + 1] = '\0';
-																                break;
-																		        }
-									    }
+								            if (sum >= 2772)
+										            {
+												                diff = sum - 2772;
+														            password[i] -= diff;
+															                password[i + 1] = '\0';
+																	            break;
+																		            }
+									        }
 
-			        printf("%s\n", password);
+				    printf("%s\n", password);
 
-				    return (0);
+				        return (0);
 }
 
